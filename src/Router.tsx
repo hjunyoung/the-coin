3e5685ec3e6238a83a3326c701872f6fbf2234d7
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-26 17:56:30 +0900

    💄 Add a favicon

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,41 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Detail from './routes/Detail';
 import Home from './routes/Home';
+import { Helmet } from 'react-helmet';
 
 const Router = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:coinId" element={<Detail />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Helmet>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Helmet>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:coinId" element={<Detail />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
