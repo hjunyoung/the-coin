abf4bad7edfd20279133d0840b3da297a493d34d
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-26 21:09:01 +0900

    🔨 Implement recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import { darkTheme, lightTheme } from './styles/theme';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Router from './Router';
+import { useRecoilValue } from 'recoil';
+import { isDarkAtom } from './atoms';
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
+  const isDark = useRecoilValue(isDarkAtom);
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
